9cc677e3fc8a12a0ff9a41ec5bc91ac18069b13c
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-05-11 13:48:19 -0300

    Initial commit

---
+++ src/main/java/com/example/SistemaDeSuperMercado/SistemaDeSuperMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaDeSuperMercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeSuperMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeSuperMercadoApplication.class, args);
+	}
+
+}
